d4f7a50b0e2a3a455fe6731f2e93dca15e121d21
GiuliaDR <94288493+GiuliaDR@users.noreply.github.com>
2022-10-21 11:08:38 +0200

    Update install_dependencies.R

--- install_dependencies.R
+++ install_dependencies.R
@@ -2,9 +2,13 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocMana
 #Install CRAN dependencies
 cran_pkgs <- c("parallel","gtools","combinat")
 cran_pkgs.inst <- cran_pkgs[!(cran_pkgs %in% rownames(installed.packages()))]
-if(length(cran_pkgs.inst)>0){
-   print(paste0("Missing ", length(cran_pkgs.inst), " CRAN Packages:"))
-   for(pkg in cran_pkgs.inst){
-     print(paste0("Installing Package:'", pkg, "'..."))
-     install.packages(pkg, repos="http://cran.rstudio.org", dependencies=TRUE)
-     print("Installed!!!")}}
+if(length(cran_pkgs.inst)>0)
+{
+  print(paste0("Missing ", length(cran_pkgs.inst), " CRAN Packages:"))
+  for(pkg in cran_pkgs.inst)
+  {
+    print(paste0("Installing Package:'", pkg, "'..."))
+    install.packages(pkg, repos="http://cran.rstudio.org", dependencies=TRUE)
+    print("Installed!!!")
+  }
+}
