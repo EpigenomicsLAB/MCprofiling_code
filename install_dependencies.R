426bd45ec07926f48e4c6b97ba70a66599404b7a
GiuliaDR <94288493+GiuliaDR@users.noreply.github.com>
2022-10-21 11:07:14 +0200

    Update install_dependencies.R

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,18 +1,10 @@
-vers <- getRversion()
-if (vers < 4.1)
-{
-  "You need R >= 4.1"
-}else{
-   if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-  #Install CRAN dependencies
-  cran_pkgs <- c("parallel","gtools","combinat")
-  cran_pkgs.inst <- cran_pkgs[!(cran_pkgs %in% rownames(installed.packages()))]
-  if(length(cran_pkgs.inst)>0){
-    print(paste0("Missing ", length(cran_pkgs.inst), " CRAN Packages:"))
-    for(pkg in cran_pkgs.inst){
-      print(paste0("Installing Package:'", pkg, "'..."))
-      install.packages(pkg, repos="http://cran.rstudio.org", dependencies=TRUE)
-      print("Installed!!!")
-    }
-  }
-}
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+#Install CRAN dependencies
+cran_pkgs <- c("parallel","gtools","combinat")
+cran_pkgs.inst <- cran_pkgs[!(cran_pkgs %in% rownames(installed.packages()))]
+if(length(cran_pkgs.inst)>0){
+   print(paste0("Missing ", length(cran_pkgs.inst), " CRAN Packages:"))
+   for(pkg in cran_pkgs.inst){
+     print(paste0("Installing Package:'", pkg, "'..."))
+     install.packages(pkg, repos="http://cran.rstudio.org", dependencies=TRUE)
+     print("Installed!!!")}
